3c150e0dc6371d99a4d837e756dcc973a7ea10b3
Joo Heredia <djjoo007uw@gmail.com>
2020-12-03 00:11:16 -0800

    add sidebar links

--- client/src/components/pages/home.js
+++ client/src/components/pages/home.js
@@ -18,14 +18,14 @@ class Home extends Component{
                 <button className="nav-toggle" aria-label="toggle navigation">
                     <span className="hamburger"></span>
                 </button>
-                <nav className="nav">
+                {/* <nav className="nav">
                     <ul className="nav__list">
                         <li className="nav__item"><a href="#home" className="nav__link">Home</a></li>
                         <li className="nav__item"><a href="#services" className="nav__link">My Information</a></li>
                         <li className="nav__item"><a href="#about" className="nav__link">About me</a></li>
                         <li className="nav__item"><a href="#work" className="nav__link">My Portfolio</a></li>
-                    </ul>
-                </nav>
+                    </ul> */}
+                {/* </nav> */}
             </header>
             
         
--- client/src/components/pages/sidebar.js
+++ client/src/components/pages/sidebar.js
@@ -1,6 +1,7 @@
 
 import React from 'react';
 import { bubble as Menu } from 'react-burger-menu';
+import { Link } from 'react-router-dom';
 
 class Sidebar extends React.Component{
   constructor(props) {
@@ -12,15 +13,36 @@ class Sidebar extends React.Component{
   return (
     <Menu>
 
-                <button className="nav-toggle" aria-label="toggle navigation">
-                    <span className="hamburger"></span>
-                </button>
-                <nav className="nav">
-                    <ul className="nav__list">
-                        <li className="nav__item"><a href="#home" className="nav__link">Home</a></li>
-                        <li className="nav__item"><a href="#services" className="nav__link">My Information</a></li>
+      <button className="nav-toggle" aria-label="toggle navigation">
+        <span className="hamburger"></span>
+      </button>
+
+        <nav className="nav">
+          <ul className="nav__list">
+              <li className="nav__item">
+                <Link to="/" className={window.location.pathname === "/" ? "nav_link active" : "nav_link"}>Home
+                </Link>
+              </li>
+
+              <li className="nav_item">
+                <Link to="/#services" className={window.location.pathname === "/#services" ? "nav_link active" : "nav_link"}>My Information
+                </Link>
+              </li>
+
+              <li className="nav_item">
+                <Link to="/#about" className={window.location.pathname === "/#about" ? "nav_link active" : "nav_link"}>About Me
+                </Link>
+              </li>
+
+              <li className="nav_item">
+                <Link to="/#work" className={window.location.pathname === "/#work" ? "nav_link active" : "nav_link"}>My Portfolio
+                </Link>
+              </li>
+{/* 
+              <li className="nav__item">
+                <a href="#services" className="nav__link">My Information</a></li>
                         <li className="nav__item"><a href="#about" className="nav__link">About me</a></li>
-                        <li className="nav__item"><a href="#work" className="nav__link">My Portfolio</a></li>
+                        <li className="nav__item"><a href="#work" className="nav__link">My Portfolio</a></li> */}
                     </ul>
                 </nav>
 
